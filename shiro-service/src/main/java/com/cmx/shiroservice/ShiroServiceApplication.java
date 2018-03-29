7e5925bcb89b35b6c7b14e71949d211cc6edc5d7
cmx <391850846@qq.com>
2018-03-29 16:46:11 +0800

    fix

--- shiro-service/src/main/java/com/cmx/shiroservice/ShiroServiceApplication.java
+++ shiro-service/src/main/java/com/cmx/shiroservice/ShiroServiceApplication.java
@@ -2,8 +2,10 @@ package com.cmx.shiroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@ImportResource("classpath:dubbo-provider.xml")
 @EnableTransactionManagement
 @SpringBootApplication
 public class ShiroServiceApplication {
